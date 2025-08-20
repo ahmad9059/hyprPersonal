5a2e31eb5ee43202f659b41d133c94ab87219f93
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-20 05:44:14 +0500

    c

--- personal.sh
+++ personal.sh
@@ -139,9 +139,9 @@ echo "${NOTE} Running dotfiles/install.sh with preset answers...${RESET}"
 cd "$HOME/dotfiles"
 chmod +x dotfile_installer.sh
 # Replace ans1 read
-# sed -i "s/read -rp \"Type 'yes' or 'no' to continue: \" ans1/ans1='yes'/g" "$HOME/dotfiles/dotfile_installer.sh"
+sed -i "s/read -rp \"Type 'yes' or 'no' to continue: \" ans1/ans1='no'/g" "$HOME/dotfiles/dotfile_installer.sh"
 # Replace ans2 read
-# sed -i "s/read -rp \"Type 'yes' or 'no' to continue: \" ans2/ans2='yes'/g" "$HOME/dotfiles/dotfile_installer.sh"
+sed -i "s/read -rp \"Type 'yes' or 'no' to continue: \" ans2/ans2='no'/g" "$HOME/dotfiles/dotfile_installer.sh"
 bash dotfile_installer.sh
 echo "${OK} Dotfiles Installation Completed${RESET}"
 
